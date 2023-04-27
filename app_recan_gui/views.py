9ac38b9d1326ddcec70f1340368d1746afb15125
yuriy babin <babin.yurii@gmail.com>
2023-04-27 11:58:28 +0300

    keep user input

--- app_recan_gui/views.py
+++ app_recan_gui/views.py
@@ -10,6 +10,10 @@ PATH_TO_MEDIA_DIR = "./media/"
 # UNCOMMENT THIS PATH IF UPLOAD ON PYTHONANYWHERE: 
 # PATH_TO_MEDIA_DIR = "/home/yuriyb/prj_recan_gui/media/"
 
+DEFAULT_PLOT_SETTINGS = {
+    "window_size" : 50,
+    "window_shift" : 25
+}
 
 SEQ_DATA = {
     "alignment": None,
@@ -17,19 +21,17 @@ SEQ_DATA = {
     "pot_rec": None,
     "pot_rec_id": None,
     "plot_div": None,
-    "window_shift": None,
     "window_size": None,
-    "default_window_size": 50,
-    "default_window_shift": 25,
+    "window_shift": None,
     "all_uploaded_files": [],  # TODO delete after ready. just to have a look at files in media dir
     "align_len": None,
-    "region_start": None,
+    "region_start": 0,
     "region_end": None,
     "dist_methods": {"pairwise distance": "pdist", 
                      "Jukes-Cantor distance": "jcd", 
                      "Kimura 2-parameter distance": "k2p",
                      "Tamura distance": "td" }, # we need dict, with values of short dist names to pass to simgen
-    "dist_method_chosen": None
+    "dist_method": "pdist"
 
 }
 
@@ -55,27 +57,32 @@ def validate_num_of_sequences(file_name):
     sim_obj = Simgen(f"{PATH_TO_MEDIA_DIR}{file_name}")
     SEQ_DATA["sequences"] = sim_obj.get_info()
     if len(SEQ_DATA["sequences"]) >= 3:
-        # put alignment length
-        SEQ_DATA["align_len"] = sim_obj.alignment_roll_window.align.get_alignment_length()
-        SEQ_DATA["pot_rec_id"] = SEQ_DATA["sequences"][0]
         return True
     else:
         return False
-        
+
+def get_default_plot_settings():
+    SEQ_DATA["window_size"] = DEFAULT_PLOT_SETTINGS["window_size"]
+    SEQ_DATA["window_shift"] = DEFAULT_PLOT_SETTINGS["window_shift"]
+    SEQ_DATA["alignment"] = None # remove file name
+    SEQ_DATA["plot_div"] = None # remove plot     
 
 def recan_view(request):
 
     if request.method == "POST" and "btn_submit_alignment" in request.POST and "alignment_file" in request.FILES:
         clean_media_dir() # remove all files from media dir
-        SEQ_DATA["alignment"] = None # remove file name
-        SEQ_DATA["plot_div"] = None # remove plot
+        get_default_plot_settings()
         uploaded_file = request.FILES["alignment_file"]
     
         if uploaded_file.name.rsplit(".")[-1] in ["fas", "fa", "fasta"]: # check extension
             save_file_in_media_dir(uploaded_file)
             file_name = uploaded_file.name
             if validate_num_of_sequences(file_name):
+                sim_obj = Simgen(f"{PATH_TO_MEDIA_DIR}{file_name}")
                 SEQ_DATA["alignment"] = file_name
+                SEQ_DATA["align_len"] = sim_obj.alignment_roll_window.align.get_alignment_length()
+                SEQ_DATA["region_end"] = SEQ_DATA["align_len"]
+                SEQ_DATA["pot_rec_id"] = SEQ_DATA["sequences"][0]
             else:
                 messages.error(request, "file contains less than three sequences")
                 SEQ_DATA["alignment"] = None
@@ -106,9 +113,14 @@ def recan_view(request):
         if plot_data.get("region_start"):
             region_start = int(plot_data.get("region_start"))
             SEQ_DATA["region_start"] = region_start
+        else:
+            SEQ_DATA["region_start"] = 0
+
         if plot_data.get("region_end"):
             region_end = int(plot_data.get("region_end"))
             SEQ_DATA["region_end"] = region_end
+        else:
+            SEQ_DATA["region_end"] = SEQ_DATA["align_len"]
         
 
         pot_rec = str(plot_data.get("pot_rec"))
@@ -133,13 +145,15 @@ def recan_view(request):
                                     dist=dist)
                 
             SEQ_DATA["plot_div"] = plot
-
+            SEQ_DATA["window_size"] = win_size
+            SEQ_DATA["window_shift"] = win_shift
         except TypeError:
-            messages.error(request, ("distance can't by calculated by chosen method:__, try to enlarge window size, window shift or both"))
-
+            messages.error(request, (f"distance can't by calculated by chosen method:{ SEQ_DATA['dist_method'] }, try to enlarge window size, window shift or both"))
+            SEQ_DATA["plot_div"] = None
         #return render(request, "base.html", context=SEQ_DATA)
     else:
         SEQ_DATA["alignment"] = None
+        
 
     #except:
     #    pass
