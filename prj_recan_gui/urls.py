745da2a1fb3fb90ce344fb4544e5d77dadf74b18
Yuriy Babin <babin.yurii@gmail.com>
2023-09-19 14:55:19 +0300

    hide admin url

--- prj_recan_gui/urls.py
+++ prj_recan_gui/urls.py
@@ -13,12 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+
+ADMIN_URL = os.environ['ADMIN_URL']
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(ADMIN_URL, admin.site.urls),
     path("", include("app_recan_gui.urls")),
 ]
 
