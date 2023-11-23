e2304a5e46fa9679eda381381f6ee924c2201105
Yuriy Babin <babin.yurii@gmail.com>
2023-11-23 19:52:28 +0300

    check if session exists, if not create a new one and return session key. in: get_session_key func

--- app_recan_gui/views.py
+++ app_recan_gui/views.py
@@ -7,6 +7,7 @@ from .models import SessionData
 from django.forms import model_to_dict
 from django.shortcuts import get_object_or_404
 from django.conf import settings
+from django.contrib.sessions.models import Session
 
 # COMMENT THIS PATH IF UPLOAD ON PYTHONANYWHERE
 #PATH_TO_MEDIA_DIR = './media/'
@@ -23,13 +24,6 @@ ERROR_MESSAGES = {'wrong_file_extension': "check file extension",
                       "If it doesn't help, change distance calculation method.")}
 
 
-def get_session_key(request):
-    if not request.session.session_key:
-        request.session.save()
-        session_key = request.session.session_key
-    else:
-        session_key = request.session.session_key
-    return session_key
 
 
 def clean_media_dir(session_key):
@@ -152,31 +146,33 @@ def get_context_from_db(session_key):
     return context
 
 
+def get_session_key(request):
+    if not request.session.session_key:
+        request.session.save()
+        session_key = request.session.session_key
+    else:
+        session_key = request.session.session_key
+        if not Session.objects.filter(session_key=session_key).exists():
+            request.session.create()
+            request.session.save()
+            session_key = request.session.session_key
+    return session_key
+
+
 def recan_view(request):
     '''main view function'''
     if request.method == 'GET':
-        #print('-------------------')
-        #print('request GET:')
-        #print(request)
         session_key = get_session_key(request)
         context = get_context_from_db(session_key)
         return render(request, 'recan.html', context=context)
     #and 'btn_submit_alignment' in request.POST 
     elif request.method == 'POST' and 'alignment_file' in request.FILES:
-        #print('-------------------')
-        #print('request POST file upload:')
-        #for i in dir(request):
-        #    print(i)
-        #print(request.POST)
-        #print(request.FILES)
         session_key = request.session.session_key
         uploaded_alignment = request.FILES['alignment_file']
         alignment_name = uploaded_alignment.name
 
         if alignment_name.rsplit('.')[-1] in ['fas', 'fa', 'fasta']:
             clean_media_dir(session_key)
-            #print('align name: ', alignment_name)
-            #print('session key: ', session_key)
             alignment_name_with_key = add_session_key_to_alignment_name(
                 alignment_name,
                 session_key)
@@ -198,13 +194,6 @@ def recan_view(request):
 
 
     elif request.method == 'POST' and 'calc_plot_form' in request.POST:
-        #print('-------------------')
-        #print('request POST plotting:')
-        #print(request)
-        #for i in dir(request):
-        #    print(i)
-        #print('request.POST:', request.POST)
-        #print('request.FILES:', request.FILES)
         session_key = request.session.session_key
         session_data = SessionData.objects.get(session_key_id=session_key)
         input_file_name = session_data.alignment_with_key
